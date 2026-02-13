b56cc738ff633c22357bbcdbc84cc894300c0dbf
charmawhorms <charmawhorms27@gmail.com>
2026-02-13 02:41:50 -0500

    made changes to the config

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     $servername = "bkr68s67yybaqejy9ptf-mysql.services.clever-cloud.com"; //Clever Cloud Host
-    $username = "uuejfcvkdxo0isxpe"; //Clever Cloud User
+    $username = "uejfcvkdxo0isxpe"; //Clever Cloud User
     $password = "bP4O5Zt83DMSaJKVXDXP";      // Your Clever Cloud Password
     $database   = "bkr68s67yybaqejy9ptf"; // Your Clever Cloud DB Name
     $port = 3306; 
