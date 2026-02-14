9678667301b036b9a54bbd93c09167d4273022f1
charmawhorms <charmawhorms27@gmail.com>
2026-02-14 15:20:52 -0500

    made changes

--- config.php
+++ config.php
@@ -1,14 +1,9 @@
 <?php
-    //$servername = "bkr68s67yybaqejy9ptf-mysql.services.clever-cloud.com"; //Clever Cloud Host
-    //$username = "uejfcvkdxo0isxpe"; //Clever Cloud User
-    //$password = "bP4O5Zt83DMSaJKVXDXP";      // Your Clever Cloud Password
-    //$database   = "bkr68s67yybaqejy9ptf"; // Your Clever Cloud DB Name
-
-    $servername = "localhost"; //Clever Cloud Host
-    $username = "root"; //Clever Cloud User
-    $password = "1234";      // Your Clever Cloud Password
-    $database   = "stockcrop";
-    //$port = 3306; 
+    $servername = "bkr68s67yybaqejy9ptf-mysql.services.clever-cloud.com"; //Clever Cloud Host
+    $username = "uejfcvkdxo0isxpe"; //Clever Cloud User
+    $password = "bP4O5Zt83DMSaJKVXDXP";      // Your Clever Cloud Password
+    $database   = "bkr68s67yybaqejy9ptf"; // Your Clever Cloud DB Name
+    $port = 3306; 
 
     // Establishing the connection
     $conn = mysqli_connect($servername, $username, $password, $database);
